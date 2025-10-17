c6fded00579461154c40543d0e7e619ee6fa8c18
Ambit Misra <111270707+invins2003@users.noreply.github.com>
2025-10-17 13:04:07 +0530

    employee filtered for registration

--- lib/module/DashBoard/Screens/DashBoardScreen.dart
+++ lib/module/DashBoard/Screens/DashBoardScreen.dart
@@ -242,8 +242,11 @@ class _DashboardscreenState extends State<Dashboardscreen> {
   void _showEmployeeSearchDialog(BuildContext context) {
     final searchController = TextEditingController();
     List<Data> filteredList = controller.employeelisttt
-        .where((emp) => emp.isActive == true)
-        .toList();
+    .where((emp) =>
+        emp.isActive == true &&
+        (emp.biometricEmpId == null ||
+         emp.biometricEmpId!.isEmpty))
+    .toList();
 
     showDialog(
       context: context,
